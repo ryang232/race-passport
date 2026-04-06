25bdb7d3e33e41a780177935c78f34cd07ecadc4
ryang232 <ryangroene0@gmail.com>
2026-04-06 18:29:37 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import CreateAccount from './pages/CreateAccount'
+import VerifyEmail from './pages/VerifyEmail'
+import BuildPassport from './pages/BuildPassport'
+import Home from './pages/Home'
+import Discover from './pages/Discover'
+import RaceDetail from './pages/RaceDetail'
+import Passport from './pages/Passport'
+import PublicProfile from './pages/PublicProfile'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/build-passport" element={<BuildPassport />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/race/:id" element={<RaceDetail />} />
+        <Route path="/passport" element={<Passport />} />
+        <Route path="/:username" element={<PublicProfile />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
