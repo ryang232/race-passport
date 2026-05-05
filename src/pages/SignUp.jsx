05ceb227b59d2b974b0467d829605121a20329e0
ryang232 <ryangroene0@gmail.com>
2026-05-05 14:49:38 -0400

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -48,7 +48,7 @@ export default function SignUp() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: `https://www.racepassportapp.com/auth/callback`,
       },
     })
     if (error) {
