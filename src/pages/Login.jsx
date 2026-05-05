a3a763dab0598e5977e5f8f6b9dfc12f536bd5b0
ryang232 <ryangroene0@gmail.com>
2026-05-05 10:14:40 -0400

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -60,7 +60,7 @@ export default function Login() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/home`,
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     })
     if (error) {
